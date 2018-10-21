08a374c186e59732f17f3a1ef45edcd453c6694c
Ranjan, Nishit (US - Bengaluru) <nishiranjan@deloitte.com>
2018-10-21 23:05:01 +0530

    channels sending and receiving messages logic added. currently just sending message for first item of the slice, not for others.

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func main() {
 		"http://golang.org",
 	}
 
+	//creating a new channel
+	c := make(chan string)
+
 	/*
 			when we use "go" infront a function call, it means run the function inside runs on a brand new go routine
 		And when that happens, and the code running inside the new Go Routine encounters a blockage, the control of the program goes back to the last Go routine.
@@ -20,22 +23,30 @@ func main() {
 
 	*/
 
+	//necessary to pass the channel to the checklink function as it is available only inside the main function and the checklink function would be able to use it only when the channel is passed to it
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	//receiving the message sent from the channel
+	fmt.Println(<-c)
 }
 
 //this method will check the status of the links present in the slice one by one
 //till the response is fetched, it won't be to the next link
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	// _is the response obj that comes along, second arg is the err
 	_, err := http.Get(link)
 
 	//if the error comes along
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		//sending message to a channel
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up")
+	c <- "Yep it's up"
 }
